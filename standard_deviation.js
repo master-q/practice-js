672e86ecad804c59a460e82929501e46ff08deda
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-10-23 13:52:06 +0900

    Add new script

---
+++ standard_deviation.js
@@ -0,0 +1,28 @@
+function average(data)
+{
+    var sum = 0;
+    for (i=0; i<data.length; i++) {
+      sum = sum + data[i];
+    }
+    return (sum / data.length);
+}
+
+function variance(data)
+{
+    var ave = average(data);
+    var varia = 0;
+    for (i=0; i<data.length; i++) {
+        varia = varia + Math.pow(data[i] - ave, 2);
+    }
+    return (varia / data.length);
+}
+
+function standard_deviation(data)
+{
+    var varia = variance(data);
+    return Math.sqrt(varia);
+}
+
+data = new Array(-3, 13, 12, -9, 51, -7);
+
+console.log('standard_deviation %d', standard_deviation(data));
